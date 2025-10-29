72a5b707862704bb8f5dd41b7201de2434d75efd
Miqueas <miqueasgigena18@gmail.com>
2025-10-29 14:47:55 -0300

    removed 2 things in tsconfig.json that weren't necessary

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { calculateBmi } from './bmiCalculator.ts';
+import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
