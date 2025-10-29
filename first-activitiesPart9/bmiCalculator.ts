04ff7bfc514fd753cf1975f029fe2523e723019a
Miqueas <miqueasgigena18@gmail.com>
2025-10-29 18:27:49 -0300

    Moved exercises 9.1-9.7 into folder first-activitiesPart9

---
+++ first-activitiesPart9/bmiCalculator.ts
@@ -0,0 +1,66 @@
+/*The BMI is defined as the body mass divided by the square of the body height, 
+and is expressed in units of kg/m2, resulting from mass in kilograms (kg) and height in metres (m).
+
+In this case we have to represent the height in cm. So the formula is 
+BMI = (mass(kg) * 10000) / (height(cm))^2
+*/
+interface BMI {
+    height: number;
+    mass: number;
+}
+
+const parseArguments = (args: string[]): BMI => {
+    if (args.length < 4) throw new Error('Not enough arguments');  // got confused with length and position, that's why it threw error
+    if (args.length > 4) throw new Error('Too many arguments');
+    
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        
+        return {
+            height: Number(args[2]),
+            mass: Number(args[3])
+        };
+    } else {
+        console.log(Number(args[3]));
+        throw new Error ('The values you provided were not numbers');
+    }
+};
+
+export const calculateBmi = (heightCm: number, massKg: number) => {
+    const BMI = (massKg * 10000) / (heightCm * heightCm);
+    let message;
+
+    if (BMI <= 18.5) { 
+        message = 'UNDER WEIGHT';
+    }
+    if (BMI > 18.5 && BMI <= 25) {
+        message = 'NORMAL RANGE';
+    }
+    if (BMI > 25 && BMI < 30) {
+        message = 'OVER WEIGHT';
+    };
+    if (BMI >= 30 && BMI < 35) {
+        message = 'OBESE';
+    };
+    if (BMI >= 35) {
+        message = 'EXTREMELY OBESE';
+    };
+
+    console.log(message);
+    return message;
+};
+
+if (require.main === module) {
+    try {
+        const {height, mass} = parseArguments(process.argv);
+        console.log(mass); // process.argv[3]
+        calculateBmi(height, mass);
+    } catch (error) {
+        let errorMessage = 'Something bad happened.';
+        if (error instanceof Error) {
+            errorMessage += ' Error: ' + error.message;
+        }
+        console.log(errorMessage);
+    }
+}
+
+
---
+++ first-activitiesPart9/exerciseCalculator.ts
@@ -0,0 +1,93 @@
+interface Values {
+    target: number;
+    periodLength: number[];
+};
+
+// arg 0 starts from 'run'
+const parseTheArguments = (args: string[]): Values => {
+    if (args.length < 4) throw new Error ('Not enough arguments');
+    
+    const arrayOfNumbers = [];
+    // iterate from position 3, that is after the target
+    for (let i = 3; i < args.length; i++) {
+        if (isNaN(Number(args[i]))) throw new Error (`"${args[i]}" is not a number, provide a number`);
+        arrayOfNumbers.push(Number(args[i]));
+    }
+
+    console.log('array of numbers:', arrayOfNumbers);
+
+    if (isNaN(Number(args[2]))) {
+        throw new Error (`"${args[2]}" is not a number, provide a number`);
+    };
+
+    return {
+        target: Number(args[2]),
+        periodLength: arrayOfNumbers
+    };
+};
+
+export const calculateExercises = (target: number, array: number[]) => {
+    let countHours = 0;
+    let average = 0;
+    let trainingDays = 0;
+    let success;
+    let ratingDescription;
+    let rating;
+
+    console.log('ARRAY LENGTH:', array.length);
+
+    for (let i = 0; i < array.length; i++) { 
+        countHours = countHours + array[i];
+        if (array[i] > 0) {
+            trainingDays = trainingDays + 1;
+        }
+    }
+
+    average = countHours / array.length;
+    console.log('average', average);
+    
+    // see if we reach the target
+    if (average < target) {
+        success = false;
+        ratingDescription = 'bad';
+        rating = 1;
+    };
+    if (average === target) {
+        success = true;
+        ratingDescription = 'You reached the target';
+        rating = 2;
+    };
+    if (average > target) {
+        success = true;
+        ratingDescription = 'You did amazing';
+        rating = 3;
+    };
+
+    const result = {
+        periodLength: array.length,
+        trainingDays: trainingDays,
+        success: success,
+        rating: rating,
+        ratingDescription: ratingDescription,
+        target: target,
+        average: average
+    };
+
+    console.log(result);
+    return result;
+};
+
+if (require.main === module) {
+    try {
+        console.log('TARGET', process.argv[2]);
+        const {target, periodLength} = parseTheArguments(process.argv);
+        calculateExercises(target, periodLength);
+    } catch (error: unknown) {
+        let errorMessage = 'Something bad happened.';
+        if (error instanceof Error) {
+            errorMessage += ' Error: ' + error.message;
+        }
+        console.log(errorMessage);
+    }
+}
+
