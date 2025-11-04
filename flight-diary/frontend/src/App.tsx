75bb2b2ad6203f4cb6ea51b1e3b5856f3b81ffa8
Miqueas <miqueasgigena18@gmail.com>
2025-11-04 17:02:29 -0300

    Exercise 9.20

--- flight-diary/frontend/src/App.tsx
+++ flight-diary/frontend/src/App.tsx
@@ -54,13 +54,31 @@ const App = () => {
     <p style={{color: 'rgb(255,0,0)'}}>{notify.data}</p>
     <form onSubmit={diaryCreation}>
       <label>date:</label>
-      <input value={newDate} onChange={(event) => (setNewDate(event.target.value))} />
+      <input type='date' value={newDate} onChange={(event) => (setNewDate(event.target.value))} />
       <br />
+      {/*WEATHER*/}
       <label>weather:</label>
-      <input value={newWeather} onChange={(event) => (setNewWeather(event.target.value))} />
+      <input type='radio' name='weather' value={'sunny'} onChange={(event) => setNewWeather(event.target.value)} />
+      <label htmlFor='sunny'>sunny</label>
+      <input type='radio' name='weather' value={'rainy'} onChange={(event) => setNewWeather(event.target.value)}/>
+      <label htmlFor='rainy'>rainy</label>
+      <input type='radio' name='weather' value={'cloudy'} onChange={(event) => setNewWeather(event.target.value)} />
+      <label htmlFor='cloudy'>cloudy</label>
+      <input type='radio' name='weather' value={'stormy'} onChange={(event) => setNewWeather(event.target.value)}/>
+      <label htmlFor='stormy'>stormy</label>
+      <input type='radio' name='weather' value={'windy'} onChange={(event) => setNewWeather(event.target.value)}/>
+      <label htmlFor='windy'>windy</label>
       <br />
+      {/*VISIBILITY*/}
       <label>visibility:</label>
-      <input value={newVisibility} onChange={(event) => (setNewVisibility(event.target.value))} />
+      <input type='radio' name='visibility' value={'great'} onChange={(event) => (setNewVisibility(event.target.value))} />
+      <label htmlFor='great'>great</label>
+      <input type='radio' name='visibility' value={'good'} onChange={(event) => (setNewVisibility(event.target.value))} />
+      <label htmlFor='good'>good</label>
+      <input type='radio' name='visibility' value={'ok'} onChange={(event) => (setNewVisibility(event.target.value))} />
+      <label htmlFor='ok'>ok</label>
+      <input type='radio' name='visibility' value={'poor'} onChange={(event) => (setNewVisibility(event.target.value))} />
+      <label htmlFor='poor'>poor</label>
       <br />
       <label>comment:</label>
       <input value={newComment} onChange={(event) => (setNewComment(event.target.value))} />
